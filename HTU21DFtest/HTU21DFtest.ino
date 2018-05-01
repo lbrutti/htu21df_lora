f7107f86a89e0dc0ca6ebcd0974bfb1344d46783
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2018-05-01 22:27:55 +0200

    aumentato sleep

--- HTU21DFtest/HTU21DFtest.ino
+++ HTU21DFtest/HTU21DFtest.ino
@@ -1,14 +1,4 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
-#include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
 
 /*************************************************** 
   This is an example for the HTU21D-F Humidity & Temp Sensor
@@ -28,26 +18,21 @@
 
 // Connect Vin to 3-5VDC
 // Connect GND to ground
-// Connect SCL to I2C clock pin (A5 on UNO)
-// Connect SDA to I2C data pin (A4 on UNO)
+// Connect SCL to I2C clock pin (D1 on NODE MCU)
+// Connect SDA to I2C data pin (DA on NODE MCU)
 
 Adafruit_HTU21DF htu = Adafruit_HTU21DF();
 SoftwareSerial sensorLoRa(12, 14); //TX, RX 
 void setup() {
   WiFi.mode(WIFI_OFF);
-//  Serial.begin(115200);
-//  Serial.println("HTU21D-F test");
   sensorLoRa.begin(9600);
 
   if (!htu.begin()) {
-//    Serial.println("Couldn't find sensor!");
     while (1);
   }
-  //  Serial.print("Temp: "); Serial.print(htu.readTemperature());
-//  Serial.print("\t\tHum: "); Serial.println(htu.readHumidity());
   sensorLoRa.print("\tT: "); sensorLoRa.print(htu.readTemperature());
   sensorLoRa.print("\tH: "); sensorLoRa.println(htu.readHumidity());
-  ESP.deepSleep(10e6);  
+  ESP.deepSleep(60e6);  
 
 }
 
